3a233e1d36dad19e02bc7677d4011029abc8f375
Evan Hughes <evanhughes3@gmail.com>
2015-04-18 13:36:56 -0700

    moved radience controller to seed file

--- solmap/app/controllers/solar_resource_controller.rb
+++ solmap/app/controllers/solar_resource_controller.rb
@@ -7,20 +7,34 @@ class SolarResourceController < ApplicationController
 
 	def data
 		base_url = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?"
-		address = params[:zip]
+		ca_state_id = 5
+		ca_counties = County.where(state_id: ca_state_id)
 		api_key = ENV['NREL_KEY']
-		# address is a placeholder NEEDS to get
-		# address from params or input
 
-		options = {
-			query: {
-				api_key: api_key,
-				address: address,
+		ca_counties.each do |county|
+			options = {
+				query: {
+					api_key: api_key,
+					address: county.sample_zipcode,
+				}
 			}
-		}
 
-		@solar_response = HTTParty.get(base_url, options)
+			@solar_response = HTTParty.get(base_url, options)
+			p outputs = @solar_response["outputs"]
+			avg_dni = outputs["avg_dni"]["annual"]
+			avg_ghi = outputs["avg_ghi"]["annual"]
+			avg_lat_tilt = outputs["avg_lat_tilt"]["annual"]
+
+			county.irradiance_dni = avg_dni
+			county.irradiance_ghi = avg_ghi
+			county.irradiance_lat_tilt = avg_lat_tilt
+			county.save!
+
+		end
+
 		render json: @solar_response
+
 	end
+
 
 end
--- solmap/db/seeds.rb
+++ solmap/db/seeds.rb
@@ -6,63 +6,64 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
+require 'httparty'
 
-states_hash =  {AL: "Alabama", 
-                AR: "Arkansas", 
-                AS: "American Samoa", 
-                AZ: "Arizona", 
-                CA: "California", 
-                CO: "Colorado", 
-                CT: "Connecticut", 
-                DC: "District of Columbia", 
-                DE: "Delaware", 
-                FL: "Florida", 
-                GA: "Georgia", 
-                GU: "Guam", 
-                HI: "Hawaii", 
-                IA: "Iowa", 
-                ID: "Idaho", 
-                IL: "Illinois", 
-                IN: "Indiana", 
-                KS: "Kansas", 
-                KY: "Kentucky", 
-                LA: "Louisiana", 
-                MA: "Massachusetts", 
-                MD: "Maryland", 
-                ME: "Maine", 
-                MI: "Michigan", 
-                MN: "Minnesota", 
-                MO: "Missouri", 
-                MS: "Mississippi", 
-                MT: "Montana", 
-                NC: "North Carolina", 
-                ND: "North Dakota", 
-                NE: "Nebraska", 
-                NH: "New Hampshire", 
-                NJ: "New Jersey", 
-                NM: "New Mexico", 
-                NV: "Nevada", 
-                NY: "New York", 
-                OH: "Ohio", 
-                OK: "Oklahoma", 
-                OR: "Oregon", 
-                PA: "Pennsylvania", 
-                PR: "Puerto Rico", 
-                RI: "Rhode Island", 
-                SC: "South Carolina", 
-                SD: "South Dakota", 
-                TN: "Tennessee", 
-                TX: "Texas", 
-                UT: "Utah", 
-                VA: "Virginia", 
-                VI: "Virgin Islands", 
-                VT: "Vermont", 
-                WA: "Washington", 
-                WI: "Wisconsin", 
-                WV: "West Virginia", 
+states_hash =  {AL: "Alabama",
+                AR: "Arkansas",
+                AS: "American Samoa",
+                AZ: "Arizona",
+                CA: "California",
+                CO: "Colorado",
+                CT: "Connecticut",
+                DC: "District of Columbia",
+                DE: "Delaware",
+                FL: "Florida",
+                GA: "Georgia",
+                GU: "Guam",
+                HI: "Hawaii",
+                IA: "Iowa",
+                ID: "Idaho",
+                IL: "Illinois",
+                IN: "Indiana",
+                KS: "Kansas",
+                KY: "Kentucky",
+                LA: "Louisiana",
+                MA: "Massachusetts",
+                MD: "Maryland",
+                ME: "Maine",
+                MI: "Michigan",
+                MN: "Minnesota",
+                MO: "Missouri",
+                MS: "Mississippi",
+                MT: "Montana",
+                NC: "North Carolina",
+                ND: "North Dakota",
+                NE: "Nebraska",
+                NH: "New Hampshire",
+                NJ: "New Jersey",
+                NM: "New Mexico",
+                NV: "Nevada",
+                NY: "New York",
+                OH: "Ohio",
+                OK: "Oklahoma",
+                OR: "Oregon",
+                PA: "Pennsylvania",
+                PR: "Puerto Rico",
+                RI: "Rhode Island",
+                SC: "South Carolina",
+                SD: "South Dakota",
+                TN: "Tennessee",
+                TX: "Texas",
+                UT: "Utah",
+                VA: "Virginia",
+                VI: "Virgin Islands",
+                VT: "Vermont",
+                WA: "Washington",
+                WI: "Wisconsin",
+                WV: "West Virginia",
                 WY: "Wyoming"}
 
-states_hash.each do |abbreviation, name| 
+states_hash.each do |abbreviation, name|
 	State.create(abbrev: abbreviation, name: name)
 end
 
@@ -75,5 +76,31 @@ hashed_zipcodes.each do |zipcode|
 	Zipcode.create(code: zipcode[:zip], city_name: zipcode[:primary_city], county_id: county.id) if county
 end
 
+# Seeding the solar radiance of counties
 
+base_url = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?"
+ca_state_id = 5
+ca_counties = County.where(state_id: ca_state_id)
+api_key = ENV['NREL_KEY']
+
+ca_counties.each do |county|
+  options = {
+          query: {
+            api_key: api_key,
+            address: county.sample_zipcode,
+    }
+  }
+
+    solar_response = HTTParty.get(base_url, options)
+    outputs = solar_response["outputs"]
+    avg_dni = outputs["avg_dni"]["annual"]
+    avg_ghi = outputs["avg_ghi"]["annual"]
+    avg_lat_tilt = outputs["avg_lat_tilt"]["annual"]
+
+    county.irradiance_dni = avg_dni
+    county.irradiance_ghi = avg_ghi
+    county.irradiance_lat_tilt = avg_lat_tilt
+    county.save!
+
+end
 
