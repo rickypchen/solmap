9f701f47d386bf2c95a6dbdbf7fdc53d6613a6a4
Evan Hughes <evanhughes3@gmail.com>
2015-04-18 13:51:57 -0700

    add migration for avg annua cost on county

--- solmap/db/schema.rb
+++ solmap/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418182806) do
+ActiveRecord::Schema.define(version: 20150418205015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150418182806) do
     t.float    "irradiance_lat_tilt"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.float    "avg_annual_cost"
   end
 
   add_index "counties", ["state_id"], name: "index_counties_on_state_id", using: :btree
