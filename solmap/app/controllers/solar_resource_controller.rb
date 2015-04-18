29b419af2a8daff8e77271bd84dbfb0d17b26954
Baffledtree <brian.c.lin2@gmail.com>
2015-04-18 08:50:46 -0700

    added dotenv-rails gem; added .env file; added nrel key to .env; added controller for solar_resource

---
+++ solmap/app/controllers/solar_resource_controller.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+class SolarResourceController < ApplicationController
+	def index
+
+	end
+
+	def get_data
+		base_url = "/api/solar/solar_resource/v1.json?"
+		address = "91803"
+		# address is a placeholder
+
+	end
+
+end
