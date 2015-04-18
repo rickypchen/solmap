aa9d636ac679876a6842794a44d3f1f6ffabb75c
Baffledtree <brian.c.lin2@gmail.com>
2015-04-18 09:54:14 -0700

    wrote tests for response, found that I needed database schema to write better tests; added params[:zip]: expect AJAX call to provide zip

--- solmap/app/controllers/solar_resource_controller.rb
+++ solmap/app/controllers/solar_resource_controller.rb
@@ -5,11 +5,26 @@ class SolarResourceController < ApplicationController
 
 	end
 
-	def get_data
-		base_url = "/api/solar/solar_resource/v1.json?"
-		address = "91803"
-		# address is a placeholder
+	def data
+		base_url = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?"
+		address = params[:zip]
+		api_key = ENV['NREL_KEY']
+		# address is a placeholder NEEDS to get
+		# address from params or input
 
+		options = {
+			query: {
+				api_key: api_key,
+				address: address,
+			}
+		}
+
+
+
+		@solar_response = HTTParty.get(base_url, options)
+		p @solar_response["outputs"]
+		p @solar_response
+		render json: @solar_response
 	end
 
 end
