19f8fad3428f5f662363b777cd29fcc673892c7b
Baffledtree <brian.c.lin2@gmail.com>
2015-04-18 09:06:20 -0700

    installed rspec and generated needed files

--- solmap/app/controllers/solar_resource_controller.rb
+++ solmap/app/controllers/solar_resource_controller.rb
@@ -8,8 +8,20 @@ class SolarResourceController < ApplicationController
 	def get_data
 		base_url = "/api/solar/solar_resource/v1.json?"
 		address = "91803"
-		# address is a placeholder
+		api_key = ENV['NREL_KEY']
+		# address is a placeholder NEEDS to get
+		# address from params or input
 
+		options = {
+			query: {
+				api_key: api_key,
+				address: address,
+			}
+		}
+
+		response = HTTParty.get(base_url, options)
+		p response
+		render json: response
 	end
 
 end
