09554d438424efc8d9d939817fa4ba9cb6eb8434
Evan Hughes <evanhughes3@gmail.com>
2015-04-18 13:55:28 -0700

    clean up seed file with new methods

--- solmap/db/seeds.rb
+++ solmap/db/seeds.rb
@@ -76,31 +76,13 @@ hashed_zipcodes.each do |zipcode|
 	Zipcode.create(code: zipcode[:zip], city_name: zipcode[:primary_city], county_id: county.id) if county
 end
 
-# Seeding the solar radiance of counties
+# Seeding the solar radiance and average cost of CA counties
 
-base_url = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?"
 ca_state_id = 5
 ca_counties = County.where(state_id: ca_state_id)
-api_key = ENV['NREL_KEY']
 
 ca_counties.each do |county|
-  options = {
-          query: {
-            api_key: api_key,
-            address: county.sample_zipcode,
-    }
-  }
-
-    solar_response = HTTParty.get(base_url, options)
-    outputs = solar_response["outputs"]
-    avg_dni = outputs["avg_dni"]["annual"]
-    avg_ghi = outputs["avg_ghi"]["annual"]
-    avg_lat_tilt = outputs["avg_lat_tilt"]["annual"]
-
-    county.irradiance_dni = avg_dni
-    county.irradiance_ghi = avg_ghi
-    county.irradiance_lat_tilt = avg_lat_tilt
-    county.save!
-
+  county.irradiance_data
+  county.genability_data
 end
 
